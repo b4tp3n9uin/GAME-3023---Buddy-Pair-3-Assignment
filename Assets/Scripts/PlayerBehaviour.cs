08b0399e69a87c2895d0a3fdaadccb078f2fbe9d
Matthew Makepeace <Matthewmakepeace32@gmail.com>
2021-11-03 23:30:45 -0400

    Added Animations to Kai

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -2,9 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum PlayerDirection
+{
+    FORWARD = 0,
+    RIGHT = 1,
+    BACKWARD = 2,
+    LEFT = 3
+}
+
 public class PlayerBehaviour : MonoBehaviour
 {
     public float speed = 5;
+    public Animator animatior;
+    public PlayerDirection direction = PlayerDirection.FORWARD;
+
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +27,36 @@ public class PlayerBehaviour : MonoBehaviour
     void Update()
     {
         transform.Translate(new Vector3(Input.GetAxis("Horizontal") * speed * Time.deltaTime, Input.GetAxis("Vertical") * speed * Time.deltaTime, 0));
+
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            direction = PlayerDirection.RIGHT;
+            animatior.SetInteger("WalkDirection", (int)direction);
+            animatior.SetBool("IsWalking", true);
+        }
+        else if (Input.GetAxis("Horizontal") < 0)
+        {
+            direction = PlayerDirection.LEFT;
+            animatior.SetInteger("WalkDirection", (int)direction);
+            animatior.SetBool("IsWalking", true);
+        }
+        else if (Input.GetAxis("Vertical") > 0)
+        {
+            direction = PlayerDirection.BACKWARD;
+            animatior.SetInteger("WalkDirection", (int)direction);
+            animatior.SetBool("IsWalking", true);
+        }
+        else if (Input.GetAxis("Vertical") < 0)
+        {
+            direction = PlayerDirection.FORWARD;
+            animatior.SetInteger("WalkDirection", (int)direction);
+            animatior.SetBool("IsWalking", true);
+        }
+        else
+        {
+            animatior.SetInteger("WalkDirection", (int)direction);
+            animatior.SetBool("IsWalking", false);
+        }
     }
 
     
