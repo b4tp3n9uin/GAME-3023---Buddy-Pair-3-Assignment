889d0683074f6f5022f3683b75d459013c4bbe2f
Matthew Makepeace <Matthewmakepeace32@gmail.com>
2021-12-06 15:27:20 -0500

    Added menu button sounds when click.

--- Assets/Scripts/SignScript.cs
+++ Assets/Scripts/SignScript.cs
@@ -24,9 +24,6 @@ public class SignScript : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            FindObjectOfType<AudioManager>().Stop("WalkNormal");
-            FindObjectOfType<AudioManager>().Stop("WalkGrass");
-
             // Display Info Message about the World.
             Time.timeScale = 0.0f;
             Debug.Log("Entered");
